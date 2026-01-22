b79db92c2d63db44eec3b044b1c93f3e14ce055a
Krish <abiramideav@gmail.com>
2026-01-22 20:13:38 +0530

    01/22/2026

---
+++ src/ex_01_JavaBasic/Lab002_HelloworldSC.java
@@ -0,0 +1,10 @@
+package ex_01_JavaBasic;
+
+public class Lab002_HelloworldSC {
+    //main -public static void- shortcut
+    //Sout
+    public static void main(String[] args) {
+
+        System.out.println("Helloworld Short cut");
+    }
+}
---
+++ src/ex_01_JavaBasic/Lab003_ShorcutHW.java
@@ -0,0 +1,12 @@
+package ex_01_JavaBasic;
+
+public class Lab003_ShorcutHW {
+
+    public static void main(String[] args) {
+        System.out.println("Abirami");
+    }
+
+    public static void main() {
+        //without parameter identifier
+        System.out.println("Abirami!");
+    }}
