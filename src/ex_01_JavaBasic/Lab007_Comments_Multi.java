db474af02ca64784d7b5189b7c4e62f356175bcc
Krish <abiramideav@gmail.com>
2026-01-22 20:21:43 +0530

    01/22/2026

---
+++ src/ex_01_JavaBasic/Lab007_Comments_Multi.java
@@ -0,0 +1,12 @@
+package ex_01_JavaBasic;
+
+public class Lab007_Comments_Multi {
+    public static void main(String[] args) {
+        /*
+         * Author : Pramod Dutta
+         * Course :  ATB10x
+         *
+         */
+        System.out.println("Multi Comment");
+    }
+}
---
+++ src/ex_01_JavaBasic/Lab008_Comments_ShortCuts.java
@@ -0,0 +1,11 @@
+package ex_01_JavaBasic;
+
+public class Lab008_Comments_ShortCuts {
+    public static void main(String[] args) {
+        // This code will print the Hi
+        // Jr QA - this will print the hi in console
+
+
+        System.out.println("Hi");
+    }
+}
